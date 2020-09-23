5197d1e08d3e7c27e9334d7ad48db8e6b5eb1095
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 22:56:47 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
   <nav>
     <img src="../assets/LOGO webmeet.svg" alt="" class="logo" />
-    <a href="/" class="link">MAIN PAGE</a>
-    <a href="/my-meetings" class="link">MY MEETINGS</a>
+    <a href="/" class="link">Main page</a>
+    <a href="/my-meetings" class="link">My meetings</a>
     <a href="" class="link login">Log in</a>
     <a href="" class="link signup">Sign up</a>
   </nav>
@@ -18,7 +18,7 @@ export default {
 nav {
   width: 100%;
   height: 5rem;
-  padding: 1rem 2rem;
+  padding: 1rem 6rem;
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -28,15 +28,41 @@ nav {
   }
 
   .link {
-    padding: 1rem;
+    padding: 0.5rem;
     margin: 0rem 1rem;
     font-size: 14px;
     text-decoration: none;
-    color: #666;
+    color: #aaa;
+    transition: all 0.4s ease;
+    font-weight: bold;
+
+    &:nth-child(2) {
+      margin-left: 4rem;
+    }
+
+    &:hover {
+      color: #000;
+      transform: translateY(-0.2rem);
+    }
   }
 
   .login {
     margin-left: auto;
+    font-weight: bold;
+  }
+
+  .signup {
+    background: #faa916;
+    color: #f6f6f6;
+    padding: 0.5rem 1.5rem;
+    border-radius: 5px;
+    font-weight: bold;
+
+    &:hover {
+      color: #faa916;
+      background: #f6f6f6;
+      box-shadow: 2px 6px 8px #0001;
+    }
   }
 }
 </style>
