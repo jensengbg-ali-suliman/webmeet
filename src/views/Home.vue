5dae40c18318016f0969214446393b915f289afb
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 22:15:50 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,24 +3,27 @@
     <Navbar />
     <div class="wrapper">
       <h1 class="logoName">webmeet.</h1>
-      <div class="container">
+      <p>
+        Expolore meeting about your favorit subjects from the comfort of your
+        home
+      </p>
+      <!-- <div class="container">
         <Meeting />
         <Meeting />
         <Meeting />
-      </div>
-      {{ meetings }}
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Meeting from '@/components/Meeting.vue'
+//import Meeting from '@/components/Meeting.vue'
 import Navbar from '@/components/Navbar.vue'
 export default {
   name: 'Home',
   components: {
-    Meeting,
+    // Meeting,
     Navbar,
   },
   created() {
@@ -55,13 +58,37 @@ export default {
 .home {
   width: 100vw;
   height: 100vh;
+  overflow-x: hidden;
 
-  .container {
-    padding: 5rem 10rem;
+  .wrapper {
+    width: 100%;
+    height: 100%;
     display: flex;
-    justify-content: space-around;
+    flex-direction: column;
+    justify-content: flex-start;
     align-items: center;
-    flex-wrap: wrap;
+    h1 {
+      margin-top: 10rem;
+      text-align: center;
+      font-family: 'Sacramento', cursive;
+      font-size: 6rem;
+    }
+
+    p {
+      text-align: center;
+      max-width: 400px;
+      color: #666;
+      font-size: 1.2rem;
+      line-height: 2rem;
+    }
+
+    .container {
+      padding: 5rem 10rem;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      flex-wrap: wrap;
+    }
   }
 }
 </style>
