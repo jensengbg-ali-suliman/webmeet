bd674445796a2d94dabcdef7cf8bf990fde9ff33
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 13:56:47 +0200

    Update Meeting.vue

--- src/components/Meeting.vue
+++ src/components/Meeting.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="wrapper">
-    <h3>This is a meeting</h3>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, non
+      libero in veritatis officia at repudiandae ipsam placeat laboriosam nobis.
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Meeting'
-};
+  name: 'Meeting',
+  created() {
+    fetch('https://api.jsonbin.io/b/5f6b3568302a837e956c0b81', {
+      method: 'GET',
+      headers: {
+        'secret-key':
+          '$2b$10$JU3Aru2zmVBe81YgmOpOdeGXs2o1wG8/zScHJo64GAltQ44l5k/qG',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  },
+}
 </script>
 
 <style lang="scss">
