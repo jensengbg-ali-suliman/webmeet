921e6ef6aa89a9ca97d9afcb38a5e3aed4ebd902
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 15:59:39 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,12 @@
   <div class="home">
     <div class="wrapper">
       <h1>This is the mainpage</h1>
-      <Meeting />
+      <div class="container">
+        <Meeting />
+        <Meeting />
+        <Meeting />
+      </div>
+      {{ meetings }}
     </div>
   </div>
 </template>
@@ -15,5 +20,45 @@ export default {
   components: {
     Meeting,
   },
+  created() {
+    this.getMeetings()
+  },
+  data: () => {
+    return {
+      meetings: 'asdasd',
+    }
+  },
+  methods: {
+    getMeetings: async function() {
+      const response = await fetch(
+        'https://api.jsonbin.io/b/5f6b3568302a837e956c0b81',
+        {
+          method: 'GET',
+          headers: {
+            'secret-key':
+              '$2b$10$JU3Aru2zmVBe81YgmOpOdeGXs2o1wG8/zScHJo64GAltQ44l5k/qG',
+          },
+        }
+      )
+      const data = await response.json()
+      console.log(data)
+      this.meetings = data
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+
+  .container {
+    padding: 5rem 10rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+}
+</style>
