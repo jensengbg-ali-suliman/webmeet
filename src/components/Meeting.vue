6d0092899cb40ae23e752bfe24f6f7234fe19369
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 13:10:32 +0200

    Create Meeting.vue

---
+++ src/components/Meeting.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <h3>This is a meeting</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Meeting'
+};
+</script>
+
+<style lang="scss">
+.wrapper {
+  h3 {
+    color: red;
+  }
+}
+</style>
