4130e1142fe4bb1da8be0b5e00d3c75d1eac3394
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 15:59:35 +0200

    Update Meeting.vue

--- src/components/Meeting.vue
+++ src/components/Meeting.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="wrapper">
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, non
-      libero in veritatis officia at repudiandae ipsam placeat laboriosam nobis.
-    </p>
+  <div class="meeting">
+    <img src="https://picsum.photos/200?random=1" alt="" />
+    <section class="info">
+      <h3>Lorem, ipsum dolor.</h3>
+      <p class="date">WED, SEP 09:00 AM</p>
+      <p class="description">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <button>more</button>
+    </section>
   </div>
 </template>
 
@@ -17,17 +22,64 @@ export default {
         'secret-key':
           '$2b$10$JU3Aru2zmVBe81YgmOpOdeGXs2o1wG8/zScHJo64GAltQ44l5k/qG',
       },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+    }).then((response) => response.json())
+    //.then((data) => console.log(data))
   },
 }
 </script>
 
-<style lang="scss">
-.wrapper {
-  h3 {
-    color: red;
+<style lang="scss" scoped>
+.meeting {
+  width: 20rem;
+  height: 25rem;
+  background: #fcfcfc;
+  border-radius: 10px;
+  transition: all 0.4s ease;
+  margin: 0rem 1rem;
+
+  img {
+    width: 100%;
+    height: 50%;
+    border-top-right-radius: 10px;
+    border-top-left-radius: 10px;
+  }
+
+  .info {
+    padding: 1rem 1.4rem;
+    width: 100%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+
+    h3 {
+      margin-top: 0.4rem;
+    }
+
+    .date {
+      color: #999;
+      margin: 1.2rem 0rem 0.8rem 0rem;
+    }
+
+    .description {
+      color: #666;
+    }
+
+    button {
+      align-self: flex-end;
+      margin-top: 1rem;
+      padding: 0.4rem 1.2rem;
+      background: #f49f0a;
+      border: none;
+      color: #fff;
+      border-radius: 4px;
+    }
+  }
+
+  &:hover {
+    transform: translateY(-10px);
+    box-shadow: 2px 6px 10px #0002;
   }
 }
 </style>
