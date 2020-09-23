042bd63b383d6eeba96641126303262028012863
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 17:04:29 +0200

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <img src="../assets/LOGO webmeet.svg" alt="" class="logo" />
+    <a href="/" class="link">MAIN PAGE</a>
+    <a href="/my-meetings" class="link">MY MEETINGS</a>
+    <a href="" class="link login">Log in</a>
+    <a href="" class="link signup">Sign up</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  width: 100%;
+  height: 5rem;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  .logo {
+    transform: scale(0.8);
+  }
+
+  .link {
+    padding: 1rem;
+    margin: 0rem 1rem;
+    font-size: 14px;
+    text-decoration: none;
+    color: #888;
+  }
+
+  .login {
+    margin-left: auto;
+  }
+}
+</style>
