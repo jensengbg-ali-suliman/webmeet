1b5364e65aacdbfcb9b44f84159cf4bd5aa6cfa8
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 13:10:13 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="wrapper">
+      <h1>This is the mainpage</h1>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Meeting from "@/components/Meeting.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Meeting
   }
 };
 </script>
