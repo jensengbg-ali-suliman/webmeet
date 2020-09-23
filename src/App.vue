c4a102d9c222f07a9519510d84e13f3c1c8196e8
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-23 17:04:03 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Sacramento&family=Source+Sans+Pro:wght@400;700&display=swap');
 * {
   margin: 0rem;
   padding: 0rem;
   box-sizing: border-box;
+  font-family: 'Source Sans Pro', sans-serif;
 }
 </style>
