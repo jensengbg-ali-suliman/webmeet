d5841c0bea20175e58c5253e5c265ef3d8fa552a
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-26 23:07:30 +0200

    Create Meetings.vue

---
+++ src/components/Meetings.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="wrapper">
+    <meetings-section meetingTag="IT - tech" :meetings="ithMeetings" />
+    <meetings-section meetingTag="Health awareness" :meetings="healthMeetings" />
+    <meetings-section meetingTag="Planning" :meetings="planningMeetings" />
+    <meetings-section meetingTag="Learning" :meetings="learningMeetings" />
+  </div>
+</template>
+
+<script>
+import MeetingsSection from "@/components/MeetingsSection.vue";
+export default {
+  name: "Meetings",
+  components: {
+    MeetingsSection
+  },
+  data: () => {
+    return {
+      meetings: [],
+      ithMeetings: [],
+      healthMeetings: [],
+      planningMeetings: [],
+      learningMeetings: []
+    };
+  },
+  created() {
+    this.getMeetings();
+  },
+  methods: {
+    getMeetings: function() {
+      fetch("https://api.jsonbin.io/b/5f6b3568302a837e956c0b81", {
+        method: "GET",
+        headers: {
+          "secret-key":
+            "$2b$10$JU3Aru2zmVBe81YgmOpOdeGXs2o1wG8/zScHJo64GAltQ44l5k/qG"
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.meetings = data;
+          this.filterMeetings(data);
+        });
+    },
+    filterMeetings: function(data) {
+      data.map(meeting => {
+        if (meeting.meetingTag === "IT-tech") {
+          this.ithMeetings.push(meeting);
+        } else if (meeting.meetingTag === "Health awareness") {
+          this.healthMeetings.push(meeting);
+        } else if (meeting.meetingTag === "Goals and planning") {
+          this.planningMeetings.push(meeting);
+        } else if (meeting.meetingTag === "Learning") {
+          this.learningMeetings.push(meeting);
+        }
+      });
+      console.log([
+        this.ithMeetings,
+        this.healthMeetings,
+        this.planningMeetings,
+        this.learningMeetings
+      ]);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  padding: 4rem 8rem;
+  width: 100%;
+  min-height: 70vh;
+}
+</style>
