f41d0a1189e72b7bf9ada7bd2f596fb0030f4c85
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-27 09:25:21 +0200

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="wrapper">
     <navbar />
-    <div class="contents">
+    <div class="content">
       <section class="textContainer">
-        <h2>welcome.</h2>
+        <h1>welcome.</h1>
         <p>start creating your account by filling in your information</p>
       </section>
       <section class="userInfo">
-        <section class="fullName">
+        <div class="fullName">
           <article>
             <label for="firstName">first name</label>
             <input type="text" v-model="firstName" />
@@ -16,16 +16,17 @@
             <label for="lastName">last name</label>
             <input type="text" v-model="lastName" />
           </article>
-        </section>
+        </div>
 
         <label for="email">your email</label>
         <input type="text" v-model="email" />
 
         <label for="password">password</label>
-        <input type="text" v-model="password" />
+        <input type="password" v-model="password" />
 
         <label for="emailConf">confirm password</label>
-        <input type="text" v-model="passwordConfirmation" />
+        <input type="password" v-model="passwordConfirmation" />
+        <button>continue</button>
       </section>
     </div>
   </div>
@@ -50,4 +51,100 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@mixin flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.wrapper {
+  .content {
+    padding: 6rem 8rem 0rem 8rem;
+    width: 100%;
+    @include flex();
+    justify-content: flex-start;
+
+    section {
+      width: 50%;
+      min-height: 60vh;
+      @include flex();
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+    }
+    .textContainer {
+      h1 {
+        font-family: "Comfortaa", cursive;
+        font-size: 2rem;
+      }
+      p {
+        color: #aaa;
+        margin-top: 0.6rem;
+        width: 36ch;
+        line-height: 1.4rem;
+      }
+    }
+
+    .userInfo {
+      @include flex();
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+      padding: 0rem 8rem 0rem 1rem;
+
+      div {
+        width: 100%;
+        @include flex();
+        justify-content: space-between;
+
+        article {
+          @include flex();
+          flex-direction: column;
+          align-items: flex-start;
+          justify-content: flex-start;
+        }
+
+        &::first-letter {
+          text-transform: uppercase;
+        }
+      }
+
+      label {
+        margin: 1.4rem 0rem 0.6rem 0rem;
+        &::first-letter {
+          text-transform: uppercase;
+        }
+      }
+
+      input {
+        width: 100%;
+        padding: 1rem 0.9rem;
+        border: none;
+        background: #f5f5f5;
+        border-radius: 6px;
+        color: #888;
+      }
+
+      button {
+        border: none;
+        padding: 0.8rem 2rem;
+        margin: 2rem 0rem;
+        align-self: flex-end;
+        color: #f6f6f6;
+        background: #faa916;
+        font-weight: bold;
+        border-radius: 5px;
+        font-size: 1rem;
+        transition: all 0.4s ease;
+        cursor: pointer;
+
+        &:hover {
+          color: #faa916;
+          background: #f6f6f6;
+          box-shadow: 2px 6px 8px #0002;
+          //transform: translateY(-0.4rem);
+        }
+      }
+    }
+  }
+}
 </style>
