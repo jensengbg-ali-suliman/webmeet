44c8cc90d1e2380a46ec26f2ad6042cc3dfde177
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-30 07:19:53 +0200

    Update MeetingView.vue

--- src/views/MeetingView.vue
+++ src/views/MeetingView.vue
@@ -4,24 +4,24 @@
 
     <div class="container">
       <section class="aboutMeeting">
-        <img src="https://picsum.photos/700/400" alt />
+        <img src="https://picsum.photos/680/350" alt />
         <article>
           <h3>about the meeting</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempora vel nulla fugiat illum dicta quia veniam quasi inventore. Asperiores consequuntur cupiditate odit ipsa quidem, laborum numquam et porro repudiandae vel soluta, corporis laboriosam praesentium. Quia possimus ullam dolorum dolore!</p>
+          <p>{{ meeting.meetingAbout }}</p>
         </article>
       </section>
       <section class="specsMeeting">
         <article class="dateSection">
           <img src="../assets/clock.svg" alt />
-          <p class="date">WED - SEP 20</p>
-          <p class="time">09:00 AM</p>
+          <p class="date">{{ meeting.meetingDate}}</p>
+          <p class="time">{{ meeting.meetingTime}}</p>
         </article>
         <article class="location">
           <img src="../assets/location.svg" alt class="locationSvg" />
           <p>Somelocation 1122, Göteborg 43 221</p>
         </article>
-        <h3>meeting detaild Name</h3>
-        <button class="cta">attend</button>
+        <h3>{{ meeting.detaildName }}</h3>
+        <button class="cta" @click="addMeetingToUser">attend</button>
       </section>
     </div>
   </div>
@@ -35,13 +35,76 @@ export default {
   },
   data: () => {
     return {
-      userID: ""
+      userID: "",
+      meetingID: "",
+      meeting: ""
     };
   },
   created() {
-    console.log(this.$route.params);
     this.userID = this.$route.params.user;
-    console.log(this.userID, "userID");
+    this.meetingID = this.$route.params.id;
+    this.getMeetingInfo(this.meetingID);
+  },
+  methods: {
+    getMeetingInfo: async function(id) {
+      let url = "https://api.jsonbin.io/b/5f6b3568302a837e956c0b81/3";
+      let API_KEY =
+        "$2b$10$JU3Aru2zmVBe81YgmOpOdeGXs2o1wG8/zScHJo64GAltQ44l5k/qG";
+      let response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "secret-key": API_KEY
+        }
+      });
+      let data = await response.json();
+      data.map(meeting => {
+        if (meeting.meetingID === id) {
+          this.meeting = meeting;
+        }
+      });
+      console.log(this.meeting);
+    },
+    addMeetingToUser: async function() {
+      let url = "https://jsonbin.org/me/users";
+      let API_KEY = "token a9affd15-2f5d-4b80-8f19-531f96900ecf";
+
+      const res = await fetch(url, {
+        method: "GET",
+        headers: {
+          authorization: API_KEY
+        }
+      });
+      let result = await res.json();
+      result.map(user => {
+        if (user.userID === this.userID) {
+          let index = result.indexOf(user);
+          console.log(index);
+          this.sendMeeting(index);
+        }
+      });
+      console.log(result);
+    },
+
+    sendMeeting: async function(i) {
+      let meetingToSend = {
+        meetingID: this.meeting.meetingID,
+        meetingName: this.meeting.meetingName,
+        meetingDate: this.meeting.meetingDate,
+        meetingTime: this.meeting.meetingTime
+      };
+      let url = "https://jsonbin.org/me/users/" + i + "/meetingsToAttend";
+      let API_KEY = "token a9affd15-2f5d-4b80-8f19-531f96900ecf";
+
+      const res = await fetch(url, {
+        method: "PATCH",
+        headers: {
+          authorization: API_KEY
+        },
+        body: JSON.stringify(meetingToSend)
+      });
+      console.log(res);
+      alert("Meeting have been added to your meetings!");
+    }
   }
 };
 </script>
@@ -79,6 +142,10 @@ export default {
         color: #666;
         width: 80%;
       }
+
+      img {
+        max-width: 100%;
+      }
     }
 
     .specsMeeting {
@@ -115,13 +182,15 @@ export default {
 
       h3 {
         margin: 2.5rem 0rem;
+        line-height: 1.8rem;
+        max-width: 14rem;
         text-transform: capitalize;
       }
 
       .cta {
         text-transform: uppercase;
         align-self: flex-end;
-        padding: 0.8rem 4rem;
+        padding: 0.6rem 3rem;
         margin-top: auto;
         border: none;
         color: #f6f6f6;
