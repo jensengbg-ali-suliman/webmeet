bcebf69b647f0f87912abed7f6bcf9e877c65951
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-09-30 16:33:58 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Comfortaa&family=Source+Sans+Pro:wght@300&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa&family=Source+Sans+Pro:wght@300&display=swap');
 * {
   margin: 0rem;
   padding: 0rem;
   box-sizing: border-box;
-  font-family: "Source Sans Pro", sans-serif;
+  font-family: 'Source Sans Pro', sans-serif;
   outline: none;
 }
 
